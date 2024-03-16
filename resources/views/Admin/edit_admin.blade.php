142a018c32b4374a4f499a255724c6c7de520697
Apasha99 <elisakds88@gmail.com>
2024-03-16 16:27:02 +0700

    button simpan

--- resources/views/Admin/edit_admin.blade.php
+++ resources/views/Admin/edit_admin.blade.php
@@ -162,7 +162,7 @@
                                 <div class="col-span-6 sm:col-full mt-4">
                                     <button
                                         class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                                        type="submit">Save</button>
+                                        type="submit">Simpan</button>
                                 </div>
                             </div>
                     </div>
